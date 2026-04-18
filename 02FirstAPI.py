27113b85695a12540f27524ff461219886ec3744
Gaurav <graj75657@gmail.com>
2026-04-18 15:49:05 +0530

    create First API

--- 02FirstAPI.py
+++ 02FirstAPI.py
@@ -5,4 +5,8 @@ app= FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "This is my first FastAPI"}
+
+@app.get("/about")
+def about():
+    return {"message": "CampusX is an education platform where you can learn AI"}
 #%%
